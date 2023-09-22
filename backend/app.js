c657dbe7968daf65332ee0aff8e8dc03cd29f113
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-09-22 14:59:32 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors'); // Importe o pacdcote cors
 
 // Configura o middleware para lidar com dados JSON
